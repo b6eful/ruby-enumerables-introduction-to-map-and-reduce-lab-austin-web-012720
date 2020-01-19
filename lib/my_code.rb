678d4f02dc81981e86981115ac1734307941e7bc
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 20:00:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,12 +59,12 @@ def reduce_to_all_true (source_array)
 end
 
 def reduce_to_any_true (source_array)
-  i = 0
-  while i < source_array.length do
-    if source_array[i]
+  k = 0
+  while k < source_array.length do
+    if source_array[k]
       return true
     end
-    i += 1
+    k += 1
   end
   return false
 end
